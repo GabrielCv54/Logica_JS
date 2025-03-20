e31ffc60b672274cc20c6d4739f775dc36efe672
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-03-20 16:57:35 -0300

    jogos eliminatorios

--- Exercicios/consultorio_dentista/script.js
+++ Exercicios/consultorio_dentista/script.js
@@ -1,10 +1,30 @@
+var pacientes=[];
+
+
 function addPaciente(){
    var patient  = document.querySelector('#paciente').value;
-   var pacientes=[];
    
+   var lista = document.getElementById('lista');
 
+    if(patient ==''){
+        alert('Deve-Se digitar o nome do paciente para agendar.')
+        console.log('Informe o nome por favor')
+    }else{ 
+    pacientes.push(patient)
+
+    var list=''
+    for(let i=0; i<pacientes.length ;i++){
+       list+= (i+1)+'°' + pacientes[i] + '\n'
+        }
+     lista.textContent = list
+  }
 }
+  //  button.addEventListener('click',addPaciente)
+
+
 
 function Urgency(){
-    var  p 
+    var patient = document.querySelector('#paciente').value;
+     
+
 }
