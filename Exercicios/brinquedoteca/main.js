fe336ae8239cb329bbcafd4ff010d85a6bd0f201
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-07-21 18:03:15 -0300

    adicionando "numeros_em_ordem",e modificando a brinquedoteca e jogos_eliminatorios

--- Exercicios/brinquedoteca/main.js
+++ Exercicios/brinquedoteca/main.js
@@ -1,20 +1,19 @@
 const funny = []
-
-
+ 
 function funnyToys(){
   var name = document.getElementById('name').value ;
   var age = document.getElementById('age').value;
-  var indexAge = funny.indexOf(age)
-  var indexName = funny.indexOf(name)
+  var sameAge = funny.some(kid => kid.idade === age);
+  var sameName = funny.some(kid => kid.criança === name);
   
   if(name === '' || age ===''){
-       alert('Esse campo (idade ou nome) deve ser preenchido')
+       window.alert('Erro! Esse campo (idade ou nome) deve ser preenchido')
   }else if(age > 12 || age < 0){
-        alert('Erro! crianças é só até os 12 anos!!')
-        funny.pop()
-  }else if(indexAge>1 && indexName > 1){
-         alert('A criança já foi adicionada!!')
-         funny.pop()
+        window.alert('Erro! crianças é só até os 12 anos!!')
+        
+  }else if(sameAge && sameName){
+         alert('Erro! A criança já foi adicionada!!')
+         return
   }else{
       funny.push({'criança':name,'idade':age});
       alert('Criança adicionada com sucesso!');
@@ -26,24 +25,85 @@ function funnyToys(){
 
 function showAll(){
    const res = document.querySelector('div#res');
+   res.textContent = ''
 
-    
-    funny.forEach(function(){
-      var result = document.createElement('pre');
-      result.innerHTML=`${funny.criança} ` + '------' + `${funny.idade} anos` + '<br>';
-      result.innerHTML='';
+   funny.forEach(function(kid){
+      var result = document.createElement('p');
+      result.textContent=`${kid.criança} ` + ' ------ ' + `${kid.idade} anos`;
       res.appendChild(result);
-    
+      
   })
-
+  
   console.log(funny)
+
+  
   
 }
 
 function filterByAge(){
-  
-}
-/* var numbers = [102,225,69,58]
-numbers.reverse()
+  /*var result = document.querySelector('div#ages');
 
-console.log(numbers.join(',')) */
+  var filter = funny.sort(function(firstElement,lastElement){
+     return (firstElement - lastElement);
+   })
+  
+   result.textContent = filter.join(',')*/
+
+  
+  var age1 = 0;
+  var age2 = 0;
+  var age3 = 0;
+  var age4 = 0;
+  var age5 = 0;
+   
+  var result = document.querySelector('div#age');
+  var result2 = document.querySelector('div#age2');
+  var result3 = document.querySelector('div#age3')
+  result.textContent = ''
+
+  funny.forEach(function(kid){
+    for(kid in funny){
+     if(kid.idade === 1){
+      const kids = []
+      age1+=1
+      kids.push(kid.criança)
+      var p = document.createElement('p');
+      p.textContent = `Crianças com 1 ano:${age1} crianças` + `${[kids.join(',')]}` + '\n'
+      result.appendChild(p)
+
+     }else if(kid.idade === 2){
+       const kids = []
+       age2+=1
+       kids.push(kid.criança)
+       let p = document.createElement('p')
+       p.textContent = `Crianças com 2 anos:${age2} crianças` + `${[kids.join(',')]}` + '\n'
+       result.appendChild(p);
+
+     }else if(kid.idade === 3){
+      const kids =[]
+      age3+=1
+      kids.push(kid.criança)
+      let p = document.createElement('p')
+      p.textContent = 'Crianças com 3 anos' +`${age4} crianças`+ `${[kids.join(',')]} ` + '\n'
+      result.appendChild(p)
+      
+     }else if(kid.idade === 4){
+      const kids =[]
+      age4+=1
+      let p = document.createElement('p')
+      kids.push(kid.criança)
+      p.textContent = 'Crianças com 4 anos:'+ `${age4}crianças`+ `${[kids.join(',')]}` + '\n'
+      result.appendChild(p)
+
+     }else if(kid.idade === 5){
+      const kids = []
+      age5+=1
+      kids.push(kid.criança)
+      let p = document.createElement('p')
+      p.textContent = 'Crianças com 5 anos:'+ `${age5}crianças`+ `${[kids.join(',')]}` + '\n'
+      result.appendChild(p)
+     }}
+  });
+
+
+}
--- Exercicios/jogos_eliminatorios/main.js
+++ Exercicios/jogos_eliminatorios/main.js
@@ -1,15 +1,44 @@
 const times = [];
 
 function addClubs(){
-   var add = String(document.getElementById('add').value );
-   var list = document.getElementById('list');
-   var table= document.getElementById('table');
-   let listagem = document.getElementById('times');
-  
-   let i = 0
-   if(add == true){ 
-      for(i;i< times.length;i++){
-        listagem.innerHTML = (i+1) + add + '<br>'
-   }
+   var add = (document.getElementById('add').value );
+   
+
+   times.push(add)
+   alert('Clube adicionado com sucesso!!')
+   console.log(times)
+
 }
+
+function listAll(){
+   var listagem = document.getElementById('times');
+   var i = 0
+   listagem.textContent = ''
+
+   times.forEach(function(time){
+      var all = document.createElement('p')
+      i++
+      all.textContent = `${i}°`+`${time}` + `\n`
+      listagem.appendChild(all)
+   
+ })
+}
+
+function tableOfGames(){
+   var allClubs = document.querySelector('div#clubs');
+   
+   
+   for(let i = 0;i<times.length;i++){
+      if(times.length % 2 == 0){
+         if(times[i] % 2==0){
+          allClubs.textContent = `${times[i]}` + '\n'
+      }else{
+         times[i] = rightClub
+      }
+ 
+      allClubs += `x ${rightClub} `
+   }else{
+         alert('Não será possível realizar os jogos, a quantidade de times não bate!!')
+      }
+   }
 }
